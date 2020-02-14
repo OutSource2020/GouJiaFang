9b75daad2487cbb9cdb7274aa8b6e5bfce0d503f
wangYulong <1269871902@qq.com>
2020-02-14 22:49:07 +0800

    Update 商户列表新增.aspx.designer.cs

--- Code df9/web/WebsiteBackstage/L1/ManagementMerchant/商户列表新增.aspx.designer.cs
+++ Code df9/web/WebsiteBackstage/L1/ManagementMerchant/商户列表新增.aspx.designer.cs
@@ -1,276 +1,305 @@
 ﻿//------------------------------------------------------------------------------
-// <自動產生的>
-//     這段程式碼是由工具產生的。
+// <自动生成>
+//     此代码由工具生成。
 //
-//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
-//     程式碼，則會遺失變更。
-// </自動產生的>
+//     对此文件的更改可能导致不正确的行为，如果
+//     重新生成代码，则所做更改将丢失。
+// </自动生成>
 //------------------------------------------------------------------------------
 
-namespace web1.WebsiteBackstage.L1.ManagementMerchant {
-    
-    
-    public partial class 商户列表新增 {
-        
-        /// <summary>
-        /// TextBox_账号信息_商户ID 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_账号信息_商户ID;
-        
-        /// <summary>
-        /// Button_随机生成商户ID 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button_随机生成商户ID;
-        
-        /// <summary>
-        /// CheckBox_手动设置商户ID 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox CheckBox_手动设置商户ID;
-        
-        /// <summary>
-        /// RadioButton_手续费收款方式_预充值 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton_手续费收款方式_预充值;
-        
-        /// <summary>
-        /// RadioButton_手续费收款方式_后充值 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton_手续费收款方式_后充值;
-        
-        /// <summary>
-        /// DropDownList_下拉框1 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList_下拉框1;
-        
-        /// <summary>
-        /// TextBox_联系人信息_姓名 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_联系人信息_姓名;
-        
-        /// <summary>
-        /// TextBox_联系人信息_联系电话 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_联系人信息_联系电话;
-        
-        /// <summary>
-        /// TextBox_联系人信息_邮箱 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_联系人信息_邮箱;
-        
-        /// <summary>
-        /// TextBox_手续费比率 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_手续费比率;
-        
-        /// <summary>
-        /// TextBox_单笔手续费 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_单笔手续费;
-        
-        /// <summary>
-        /// TextBox_充值最低手续费 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_充值最低手续费;
-        
-        /// <summary>
-        /// TextBox_充值最低余额 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_充值最低余额;
-        
-        /// <summary>
-        /// TextBox_提款最低单笔金额 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_提款最低单笔金额;
-        
-        /// <summary>
-        /// TextBox_提款最高单笔金额 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_提款最高单笔金额;
-        
-        /// <summary>
-        /// TextBox_第一阶梯起 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第一阶梯起;
-        
-        /// <summary>
-        /// TextBox_第一阶梯止 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第一阶梯止;
-        
-        /// <summary>
-        /// TextBox_第一阶梯百分比 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第一阶梯百分比;
-        
-        /// <summary>
-        /// TextBox_第二阶梯起 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第二阶梯起;
-        
-        /// <summary>
-        /// TextBox_第二阶梯止 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第二阶梯止;
-        
-        /// <summary>
-        /// TextBox_第二阶梯百分比 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第二阶梯百分比;
-        
-        /// <summary>
-        /// TextBox_第三阶梯起 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第三阶梯起;
-        
-        /// <summary>
-        /// TextBox_第三阶梯止 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第三阶梯止;
-        
-        /// <summary>
-        /// TextBox_第三阶梯百分比 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第三阶梯百分比;
-        
-        /// <summary>
-        /// TextBox_第四阶梯起 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第四阶梯起;
-        
-        /// <summary>
-        /// TextBox_第四阶梯止 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第四阶梯止;
-        
-        /// <summary>
-        /// TextBox_第四阶梯百分比 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox_第四阶梯百分比;
-        
-        /// <summary>
-        /// Button_返回 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button_返回;
-        
-        /// <summary>
-        /// Button_新增商户 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button_新增商户;
-    }
+namespace web1.WebsiteBackstage.L1.ManagementMerchant
+{
+
+
+  public partial class 商户列表新增
+  {
+
+    /// <summary>
+    /// TextBox_账号信息_商户ID 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_账号信息_商户ID;
+
+    /// <summary>
+    /// Button_随机生成商户ID 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Button Button_随机生成商户ID;
+
+    /// <summary>
+    /// CheckBox_手动设置商户ID 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.CheckBox CheckBox_手动设置商户ID;
+
+    /// <summary>
+    /// RadioButton_手续费收款方式_预充值 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.RadioButton RadioButton_手续费收款方式_预充值;
+
+    /// <summary>
+    /// RadioButton_手续费收款方式_后充值 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.RadioButton RadioButton_手续费收款方式_后充值;
+
+    /// <summary>
+    /// DropDownList_下拉框1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.DropDownList DropDownList_下拉框1;
+
+    /// <summary>
+    /// DropDownList_手动提款启用状态 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.DropDownList DropDownList_手动提款启用状态;
+
+    /// <summary>
+    /// DropDownList_API提款启用状态 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.DropDownList DropDownList_API提款启用状态;
+
+    /// <summary>
+    /// DropDownList_二步验证 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.DropDownList DropDownList_二步验证;
+
+    /// <summary>
+    /// TextBox_联系人信息_姓名 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_联系人信息_姓名;
+
+    /// <summary>
+    /// TextBox_联系人信息_联系电话 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_联系人信息_联系电话;
+
+    /// <summary>
+    /// TextBox_联系人信息_邮箱 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_联系人信息_邮箱;
+
+    /// <summary>
+    /// TextBox_手续费比率 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_手续费比率;
+
+    /// <summary>
+    /// TextBox_单笔手续费 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_单笔手续费;
+
+    /// <summary>
+    /// TextBox_充值最低手续费 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_充值最低手续费;
+
+    /// <summary>
+    /// TextBox_充值最低余额 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_充值最低余额;
+
+    /// <summary>
+    /// TextBox_提款最低单笔金额 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_提款最低单笔金额;
+
+    /// <summary>
+    /// TextBox_提款最高单笔金额 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_提款最高单笔金额;
+
+    /// <summary>
+    /// TextBox_第一阶梯起 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第一阶梯起;
+
+    /// <summary>
+    /// TextBox_第一阶梯止 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第一阶梯止;
+
+    /// <summary>
+    /// TextBox_第一阶梯百分比 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第一阶梯百分比;
+
+    /// <summary>
+    /// TextBox_第二阶梯起 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第二阶梯起;
+
+    /// <summary>
+    /// TextBox_第二阶梯止 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第二阶梯止;
+
+    /// <summary>
+    /// TextBox_第二阶梯百分比 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第二阶梯百分比;
+
+    /// <summary>
+    /// TextBox_第三阶梯起 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第三阶梯起;
+
+    /// <summary>
+    /// TextBox_第三阶梯止 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第三阶梯止;
+
+    /// <summary>
+    /// TextBox_第三阶梯百分比 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第三阶梯百分比;
+
+    /// <summary>
+    /// TextBox_第四阶梯起 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第四阶梯起;
+
+    /// <summary>
+    /// TextBox_第四阶梯止 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第四阶梯止;
+
+    /// <summary>
+    /// TextBox_第四阶梯百分比 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_第四阶梯百分比;
+
+    /// <summary>
+    /// Button_返回 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Button Button_返回;
+
+    /// <summary>
+    /// Button_新增商户 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Button Button_新增商户;
+  }
 }
