1d3ded00304570dec35d31910b455d1ea0fc5f9a
ddxl <1376775835@qq.com>
2020-03-26 20:16:45 +0800

    添加延时

--- Code df9/web/WebsiteMerchant/MerchantOrder/商户提款导入Excel.aspx.cs
+++ Code df9/web/WebsiteMerchant/MerchantOrder/商户提款导入Excel.aspx.cs
@@ -343,6 +343,9 @@ namespace web1.WebsiteMerchant.商户订单
             Button_确认订单发起.Enabled = false;//防止重复操作
             long OperatorId = GetTimeStamp();
             int Sindex = 0;
+            string Cookie_UserName = ClassLibrary1.ClassAccount.检查商户端cookie2();
+            Mutex m = new Mutex(false, Cookie_UserName);
+            m.WaitOne();
             using (SqlSugarClient dbClient = new DBClient().GetClient())
             {
                 for (int i = 0; i < Gridview1.Rows.Count; i++)
@@ -359,7 +362,6 @@ namespace web1.WebsiteMerchant.商户订单
                         continue;
                     }
 
-                    string Cookie_UserName = ClassLibrary1.ClassAccount.检查商户端cookie2();
                     dbClient.Ado.UseTran(() => { }); // select 之前保证一次 commit，即使什么都不做
                     table_商户账号 record = null;
                     dbClient.Ado.UseTran(() =>
@@ -456,8 +458,10 @@ namespace web1.WebsiteMerchant.商户订单
 
                         dbClient.Insertable(detail).ExecuteCommand();
                     });
+                    Thread.Sleep(100);
                 }
             }
+            m.ReleaseMutex();
             Response.Redirect("./商户提款记录.aspx");
         }
 
