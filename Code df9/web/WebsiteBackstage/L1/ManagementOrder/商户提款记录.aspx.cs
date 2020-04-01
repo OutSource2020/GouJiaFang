844837f8adbc74dbe23180ab1728a5e72d41714a
ddxl <1376775835@qq.com>
2020-04-01 20:57:41 +0800

    去除自动发送回调

--- Code df9/web/WebsiteBackstage/L1/ManagementOrder/商户提款记录.aspx.cs
+++ Code df9/web/WebsiteBackstage/L1/ManagementOrder/商户提款记录.aspx.cs
@@ -1295,7 +1295,7 @@ namespace web1.WebsiteBackstage.L1.ManagementOrder
 
             }
             勾选循环全部取消();
-            Button_发送近三天订单回调_Click(null, null);
+            // Button_发送近三天订单回调_Click(null, null);
         }
 
         private void DeleteRecord(string CustomerID)
